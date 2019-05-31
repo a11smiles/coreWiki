c487e03cf2b83317d64486bd012bc52ab98456a6
Joshua Davis <me@jdav.is>
2019-05-30 20:58:19 -0400

    added material to modules

--- coreWiki/ClientApp/src/app/app.module.ts
+++ coreWiki/ClientApp/src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
